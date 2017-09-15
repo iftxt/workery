4e8be58b37d4854363859233d7b31ce2fe82a485
Daijiro Wachi <daijiro.wachi@gmail.com>
2017-09-15 15:28:00 +0200

    feat(cluster): use cluster to not share the modules

--- index.ts
+++ index.ts
@@ -1,4 +1,5 @@
 import * as amqp from 'amqplib'
+import * as cluster from 'cluster'
 import * as deepFreeze from 'deep-freeze'
 import * as dotenv from 'dotenv'
 import { runInNewContext } from 'vm'
@@ -8,21 +9,28 @@ dotenv.config()
 const queue = process.env.AMQP_QUEUE as string
 const open = amqp.connect(process.env.AMQP_ENDPOINT as string)
 
-open.then((conn) => conn.createChannel()).then((ch) =>
-  ch.assertQueue(queue).then((ok) =>
-    ch.consume(queue, (msg) => {
-      if (msg) {
-        ch.ack(msg)
-        try {
-          const message = JSON.parse(msg.content.toString())
-          runInNewContext(message.code, {
-            console: deepFreeze({ ...require('console') }),
-            http: deepFreeze({ ...require('http') }),
-          }, { filename: 'sandboxed.js', timeout: 1e5, displayErrors: true })
-        } catch (err) {
-          console.warn.apply(console, err)
+if (cluster.isMaster) {
+  const worker = cluster.fork()
+  open.then((conn) => conn.createChannel()).then((ch) =>
+    ch.assertQueue(queue).then((ok) =>
+      ch.consume(queue, (msg) => {
+        if (msg) {
+          ch.ack(msg)
+          worker.send(msg.content.toString())
         }
-      }
-    }),
-  ),
-).catch(console.warn)
+      }),
+    ),
+  ).catch(console.warn)
+} else {
+  process.on('message', (msg) => {
+    try {
+      const message = JSON.parse(msg)
+      runInNewContext(message.code, {
+        console: deepFreeze({ ...require('console') }),
+        http: deepFreeze({ ...require('http') }),
+      }, { filename: 'sandboxed.js', timeout: 1e5, displayErrors: true })
+    } catch (err) {
+      console.warn.apply(console, err)
+    }
+  })
+}
