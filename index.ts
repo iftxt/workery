7cc87164478be8973ec77f3e83d82662ba21cfa6
Daijiro Wachi <daijiro.wachi@gmail.com>
2017-09-15 21:57:53 +0200

    [wip]: add the proxy to the http

--- index.ts
+++ index.ts
@@ -3,34 +3,62 @@ import * as cluster from 'cluster'
 import * as deepFreeze from 'deep-freeze'
 import * as dotenv from 'dotenv'
 import { runInNewContext } from 'vm'
+import * as http from 'http'
 
 dotenv.config()
 
 const queue = process.env.AMQP_QUEUE as string
 const open = amqp.connect(process.env.AMQP_ENDPOINT as string)
 
-if (cluster.isMaster) {
-  const worker = cluster.fork()
-  open.then((conn) => conn.createChannel()).then((ch) =>
-    ch.assertQueue(queue).then((ok) =>
-      ch.consume(queue, (msg) => {
-        if (msg) {
-          ch.ack(msg)
-          worker.send(msg.content.toString())
-        }
-      }),
-    ),
-  ).catch(console.warn)
-} else {
-  process.on('message', (msg) => {
-    try {
-      const message = JSON.parse(msg)
-      runInNewContext(message.code, {
-        console: deepFreeze({ ...require('console') }),
-        http: deepFreeze({ ...require('http') }),
-      }, { filename: 'sandboxed.js', timeout: 1e5, displayErrors: true })
-    } catch (err) {
-      console.warn.apply(console, err)
-    }
-  })
+const recursiveCall = (names, obj) => {
+  const keys = names.split('.')
+  if (keys.length === 1) {
+    return obj[keys[0]]
+  }
+  const key = keys.splice(0, 1)
+  return recursiveCall(keys.join('.'), obj[key])
 }
+
+const _handler = (prev) => {
+  return {
+    get: function (target, name) {
+      if (typeof http[name] === 'object') {
+        if (prev) _prev += '.' + name
+        return prox(http[name], handler(prev))
+      }
+      return recursiveCall(prev + '.' + name, http)
+    },
+    set: () => {
+      return true
+    }
+  }
+}
+
+function prox (obj, handler) {
+  return new Proxy(obj, handler)
+}
+
+const _http = new prox({}, {
+  get: function(target, name) {
+    return prox(http[name], _handler(name))
+  }
+})
+
+open.then((conn) => conn.createChannel()).then((ch) => {
+  ch.assertQueue(queue).then((ok) => {
+    ch.consume(queue, (msg) => {
+      if (msg) {
+        ch.ack(msg)
+        try {
+          const message = JSON.parse(msg.content.toString())
+          runInNewContext(message.code, {
+            console: deepFreeze({ ...require('console') }),
+            http: _http,
+          }, { filename: 'sandboxed.js', timeout: 1e5, displayErrors: true })
+        } catch (err) {
+          console.warn.apply(console, err)
+        }
+      }
+    })
+  })
+}).catch(console.warn)
