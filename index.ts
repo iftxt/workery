b6ea0d9c03dbc570121d69b6a17d8dbe92c85db0
Daijiro Wachi <daijiro.wachi@gmail.com>
2017-09-16 23:05:25 +0200

    fix(http, https): fix ts warning

--- index.ts
+++ index.ts
@@ -1,51 +1,13 @@
 import * as amqp from 'amqplib'
 import * as cluster from 'cluster'
-import * as deepFreeze from 'deep-freeze'
 import * as dotenv from 'dotenv'
-import * as http from 'http'
+import sandRequire = require('sand-require')
 import { runInNewContext } from 'vm'
 
 dotenv.config()
 
 const queue = process.env.AMQP_QUEUE as string
 const open = amqp.connect(process.env.AMQP_ENDPOINT as string)
-
-const recursiveCall = (names: string, obj: object): any => {
-  const keys = names.split('.')
-  if (keys.length === 1) {
-    return (obj as any)[keys[0]]
-  }
-
-  const key = keys.splice(0, 1)[0]
-  return recursiveCall(keys.join('.'), (obj as any)[key])
-}
-
-const handler = (prev: string) => {
-  return {
-    get(target: object, name: string) {
-      if (typeof (http as any)[name] === 'object') {
-        if (prev) {
-          prev += '.' + name
-        }
-        return prox((http as any)[name], handler(prev))
-      }
-      return recursiveCall(prev + '.' + name, http)
-    },
-    set: () => {
-      return true
-    },
-  }
-}
-
-function prox(obj: object, callback: object) {
-  return new Proxy(obj, callback)
-}
-
-const proxedHttp = prox({}, {
-  get(target: object, name: string) {
-    return prox((http as any)[name], handler(name))
-  },
-})
 
 open.then((conn) => conn.createChannel()).then((ch) => {
   ch.assertQueue(queue).then((ok) => {
@@ -55,8 +17,9 @@ open.then((conn) => conn.createChannel()).then((ch) => {
         try {
           const message = JSON.parse(msg.content.toString())
           runInNewContext(message.code, {
-            console: deepFreeze({ ...require('console') }),
-            http: proxedHttp,
+            console: sandRequire('console'),
+            http: sandRequire('http'),
+            https: sandRequire('https'),
           }, { filename: 'sandboxed.js', timeout: 1e5, displayErrors: true })
         } catch (err) {
           console.warn.apply(console, err)
---
+++ types/sand-require.d.ts
@@ -0,0 +1,4 @@
+declare module 'sand-require' {
+	function sr<T>(name: string): any;
+	export = sr;
+}
